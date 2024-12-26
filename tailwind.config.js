05dfd656d31c9370cdf71f01d3ff455fd2e215f7
Said Azmour <azmour2016maroc@gmail.com>
2024-12-26 17:28:00 +0100

    add all sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}",
-      "./node_modules/flowbite/**/*.js"
-  ],
+  content: ["./dist/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     container: {
       center: true,
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
+        md: '3rem',
         lg: '4rem',
-        xl: '5rem',
+        xl: '3rem',
         '2xl': '6rem',
-    }
+      },
     },
     extend: {
+      scrollbar: {
+        hide: {
+          '&::-webkit-scrollbar': { display: 'none' },
+          '&': { 'scrollbar-width': 'none' },
+        },
+      },
       backgroundImage: {
         'right-content': "url('/src/imags/icons/Right-Content.png')",
         'left-content': "url('/src/imags/icons/VectorHeader.png')",
-      },    
+      },
       colors: {
         primary70: '#3DCBB1',
         secondary: '#FFD130',
@@ -26,9 +31,9 @@ module.exports = {
         primary60: '#E56D67',
         primary80: '#C42A22',
         primary100: '#98211A5',
-        black90 : '#1B1B1BE5',
-        black60 : '#1B1B1B99',
-        black30 : '#1B1B1B1A',
+        black90: '#1B1B1BE5',
+        black60: '#1B1B1B99',
+        black30: '#1B1B1B1A',
         white90: '#F9F9F9E5',
         white60: '#F9F9F999',
         white30: '#F9F9F94D',
@@ -36,20 +41,19 @@ module.exports = {
         warning: '#A04AE3',
         success: '#00DDC0',
         primary: '#00DDC0',
-
-        },
+      },
       fontSize: {
-        xs: ['6px',{lineHeight: '9px'}],
-        sm: ['8px',{lineHeight: '12px'}],
-        base: ['10px',{lineHeight: '15px'}],
-        md: ['14px',{lineHeight: '18px'}],
-        lg: ['16px',{lineHeight: '24px'}],
-        xl: ['20px',{lineHeight: '30px'}],
-        '2xl': ['24px',{lineHeight: '36px'}],
-        '3xl': ['32px',{lineHeight: '48px'}],
-        '4xl': ['40px',{lineHeight: '60px'}],
-        '5xl': ['48px',{lineHeight: '72px'}],
-        '6xl': ['61px',{lineHeight: '92px'}],
+        xs: ['6px', { lineHeight: '9px' }],
+        sm: ['8px', { lineHeight: '12px' }],
+        base: ['10px', { lineHeight: '15px' }],
+        md: ['14px', { lineHeight: '18px' }],
+        lg: ['16px', { lineHeight: '24px' }],
+        xl: ['20px', { lineHeight: '30px' }],
+        '2xl': ['24px', { lineHeight: '36px' }],
+        '3xl': ['32px', { lineHeight: '48px' }],
+        '4xl': ['40px', { lineHeight: '60px' }],
+        '5xl': ['48px', { lineHeight: '72px' }],
+        '6xl': ['61px', { lineHeight: '92px' }],
       },
       fontFamily: {
         gilroy: ['gilroy', 'sans-serif'],
@@ -57,8 +61,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin')
-  
-]
-}
-
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
+};
